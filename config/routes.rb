cf8b29dfac6e10c3103e2c3d1d44bc875d0a3b1a
Waran Nadarajah <waran.nada@gmail.com>
2016-02-23 15:45:30 -0500

    Create home, about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get '/home' => 'pages#home'
+
   get '/about' => 'pages#about'
 
   get '/help' => 'pages#help'
 
-  get 'contact' =>  'pages#contact'
+  get '/contact' =>  'pages#contact'
 
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,19 @@ class PagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About Us | Twttr"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Twttr"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
+  	assert_select "title", "Contact Us | Twttr"
   end
 
 end
