0998f5876980425d5463514034c7733cdfa21baa
Waran Nadarajah <waran.nada@gmail.com>
2016-02-23 17:13:00 -0500

    Add minitest helpers

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  
+  def setup
+  	@page_title = "Twttr"
+  end
+
+  test "should get home" do
+  	get :home
+  	assert_response :success
+  	assert_select "title", "Your window to the world | #{@page_title}"
+  end
+
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About Us | Twttr"
+    assert_select "title", "About Us | #{@page_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Twttr"
+    assert_select "title", "Help | #{@page_title}"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-  	assert_select "title", "Contact Us | Twttr"
+  	assert_select "title", "Contact Us | #{@page_title}"
   end
 
 end
