113a19c4fb814bae925844a698a70caaec732565
Waran Nadarajah <waran.nada@gmail.com>
2016-02-24 09:42:48 -0500

    Add page title and write test cases

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 class PagesControllerTest < ActionController::TestCase
   
   def setup
-  	@page_title = "Twttr"
+  	@base_title = "Twttr"
   end
 
   test "should get home" do
   	get :home
   	assert_response :success
-  	assert_select "title", "Your window to the world | #{@page_title}"
+  	assert_select "title", "Welcome | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About Us | #{@page_title}"
+    assert_select "title", "About Us | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@page_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-  	assert_select "title", "Contact Us | #{@page_title}"
+  	assert_select "title", "Contact Us | #{@base_title}"
   end
 
 end
