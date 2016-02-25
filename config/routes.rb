4a800cdb8a6d0da66a7738d2a342c8471f0a4934
Waran Nadarajah <waran.nada@gmail.com>
2016-02-25 10:52:18 -0500

    Add link path to footer and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get '/home' => 'pages#home'
 
-  get '/about' => 'pages#about'
+  get 'home' => 'pages#home'
 
-  get '/help' => 'pages#help'
+  get 'about' => 'pages#about'
 
-  get '/contact' =>  'pages#contact'
+  get 'help' => 'pages#help'
+
+  get 'contact' =>  'pages#contact'
 
 end
