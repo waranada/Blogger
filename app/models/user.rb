a9c76a5f9214fd314bb1ff4d1ff9c2dd8681272f
Waran Nadarajah <waran.nada@gmail.com>
2016-02-29 21:53:27 -0500

    Write user model test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	validates :full_name, length: { maximum: 25 }, presence: true
+	validates :username, length: { maximum: 12 }, presence: true
+	validates :email, length: { maximum: 244 }, presence: true
 end
+
+
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  	@user = User.new(full_name: "Example User", username: "exampleuser", email: "user@example.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "fullname should be present" do
+  	@user.full_name = "   "
+  	assert_not @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "  "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "fullname should not be too long" do
+    @user.full_name = "a" * 26
+    assert_not @user.valid?
+  end
+
+  test "username should not be too long" do
+    @user.username = "a" * 13
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 245
+    assert_not @user.valid?
+  end
 end
