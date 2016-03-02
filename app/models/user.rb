19e367f6949352f8d22fa96aec7791073bb65e97
Waran Nadarajah <waran.nada@gmail.com>
2016-03-01 20:17:26 -0500

    Add a test for the email downcasing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 	validates :full_name, length: { maximum: 25 }, presence: true
 	validates :username, length: { maximum: 12 }, presence: true
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, length: { maximum: 244 }, presence: true,
 					  format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,7 +52,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                            foo@bar_baz.com foo@bar+baz.com]
+                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -66,8 +66,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "TesT@ExAmple.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 8
+    @user.password = @user.password_confirmation = " " * 7
     assert_not @user.valid?
   end
 
@@ -75,18 +82,4 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = " " * 7
     assert_not @user.valid?
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
