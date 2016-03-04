298ea38aa47e10800b65d877bf3b9082987c71b1
Waran Nadarajah <waran.nada@gmail.com>
2016-03-04 18:32:10 -0500

    New updates with rails 5.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#Handle a successful save.
+  	else
+  		render 'new'
+  	end
+  end
+
+
+  private
+  	def user_params
+  		params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation)
+  	end
+
 end
