bb7dcd96c1188decfa92d4557e1657d8e4115d0b
Waran Nadarajah <waran.nada@gmail.com>
2016-03-05 13:17:37 -0500

    Finish user sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		#Handle a successful save.
+      flash[:success] = "Welcome to Blogger!"
+      redirect_to @user
   	else
   		render 'new'
   	end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	test "invalid signup information" do
+		get signup_path
+		assert_no_difference 'User.count' do
+			post users_path, user: { full_name: "",
+									username: "testuser",
+									email: "testuser@example",
+									password: ",.//.,",
+									password_confirmation: ",.//.,,./" }
+		end
+		assert_template 'users/new'
+	end
+
+	test "valid signup information" do
+		get signup_path
+		assert_difference 'User.count', 1 do
+			post_via_redirect users_path, user: { full_name: "Test User",
+												 username: "testuser",
+												 email: "testuser@example.com",
+												 password: ",.//.,,./",
+												 password_confirmation: ",.//.,,./" }
+		end
+		assert_template 'users/show'
+	end
+
+
+end
